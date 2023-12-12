6f9dd6c7f4d87a2acb53205d3ece017e7966eedb
Pettzr <petersong2003@hotmail.com>
2023-12-12 17:34:13 -0300

    first commit

---
+++ videos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class InfoVideos (models.Model):
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(null=False,blank=False)
+    url = models.CharField(max_length=100, null=False, blank=False)
